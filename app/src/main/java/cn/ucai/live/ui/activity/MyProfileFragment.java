0fbdf4df783a44606b800b20fb6065b1e6fd773a
tangchuanzhong <1319902136@qq.com>
2017-02-28 11:15:31 +0800

    退出方法的编写

--- app/src/main/java/cn/ucai/live/ui/activity/MyProfileFragment.java
+++ app/src/main/java/cn/ucai/live/ui/activity/MyProfileFragment.java
@@ -13,9 +13,12 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
+import cn.ucai.live.LiveHelper;
 import cn.ucai.live.data.model.LiveSettings;
 
 import cn.ucai.live.R;
+import cn.ucai.live.utils.MFGT;
+
 import com.hyphenate.EMCallBack;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.easeui.utils.EaseUserUtils;
@@ -72,11 +75,12 @@ public class MyProfileFragment extends Fragment {
     }
 
     @OnClick(R.id.btn_logout) void onLogout(){
-        EMClient.getInstance().logout(false, new EMCallBack() {
+        LiveHelper.getInstance().logout(false, new EMCallBack() {
             @Override
             public void onSuccess() {
                 getActivity().finish();
-                startActivity(new Intent(getActivity(), LoginActivity.class));
+                MFGT.gotoLoginCleanTask(getActivity());
+                //startActivity(new Intent(getActivity(), LoginActivity.class));
             }
 
             @Override
